fd67f1ba932e36e70315314e073f98fc34ff1815
DilanBuitrago25 <dabuitrago364@soy.sena.edu.co>
2024-05-20 12:30:02 -0500

    cambios asistencia

---
+++ AssitADSOproyect/AssitADSOproyect/Controllers/AsistenciasController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ClaseDatos;
+
+namespace AssitADSOproyect.Controllers
+{
+    public class AsistenciasController : Controller
+    {
+        private BDAssistsADSOEntities db = new BDAssistsADSOEntities();
+
+        // GET: Asistencias
+        public ActionResult Index()
+        {
+            var asistencia = db.Asistencia.Include(a => a.Usuario);
+            return View(asistencia.ToList());
+        }
+
+        // GET: Asistencias/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Asistencia asistencia = db.Asistencia.Find(id);
+            if (asistencia == null)
+            {
+                return HttpNotFound();
+            }
+            return View(asistencia);
+        }
+
+        // GET: Asistencias/Create
+        public ActionResult Create()
+        {
+            ViewBag.Id_usuario = new SelectList(db.Usuario, "Id_usuario", "Tipo_Documento_usuario");
+            return View();
+        }
+
+        // POST: Asistencias/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id_asistencia,Tipo_asistencia,fecha_asistencia,Hora_asistencia,Id_usuario")] Asistencia asistencia)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Asistencia.Add(asistencia);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Id_usuario = new SelectList(db.Usuario, "Id_usuario", "Tipo_Documento_usuario", asistencia.Id_usuario);
+            return View(asistencia);
+        }
+
+        // GET: Asistencias/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Asistencia asistencia = db.Asistencia.Find(id);
+            if (asistencia == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Id_usuario = new SelectList(db.Usuario, "Id_usuario", "Tipo_Documento_usuario", asistencia.Id_usuario);
+            return View(asistencia);
+        }
+
+        // POST: Asistencias/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id_asistencia,Tipo_asistencia,fecha_asistencia,Hora_asistencia,Id_usuario")] Asistencia asistencia)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(asistencia).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Id_usuario = new SelectList(db.Usuario, "Id_usuario", "Tipo_Documento_usuario", asistencia.Id_usuario);
+            return View(asistencia);
+        }
+
+        // GET: Asistencias/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Asistencia asistencia = db.Asistencia.Find(id);
+            if (asistencia == null)
+            {
+                return HttpNotFound();
+            }
+            return View(asistencia);
+        }
+
+        // POST: Asistencias/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Asistencia asistencia = db.Asistencia.Find(id);
+            db.Asistencia.Remove(asistencia);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
